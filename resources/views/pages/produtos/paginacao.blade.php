6fa4892ad88a0764161eae7947948c4d9021e051
Eduardo <edu.elias2004@gmail.com>
2024-06-26 16:39:39 -0300

    import jquery e blockUI

--- resources/views/pages/produtos/paginacao.blade.php
+++ resources/views/pages/produtos/paginacao.blade.php
@@ -6,12 +6,15 @@
     </div>
 
     <div>
-        <form action="">
+        <form action="{{ route('produto.index') }}">
             <input type="text" name="pesquisar" placeholder="Digite o nome">
             <button> Pesquisar </button>
             <a type="button" href="" class="btn btn-success float-end"> Incluir Produto</a>
         </form>
         <div class="table-responsive small mt-4">
+          @if ($findProduto->isEmpty())
+              <p> Não existe dados.</p>
+          @else
             <table class="table table-striped table-sm">
                <thead>
                  <tr>
@@ -27,12 +30,13 @@
                     <td>{{ 'R$' . ' ' . number_format($produto->valor, 2, ',', '.') }}</td>
                     <td> 
                         <a href="" class="btn btn-light btn-sm">Editar</a>
-                        <a href="" class="btn btn-danger btn-sm">Excluir</a>
+                        <a href="{{ route('produto.delete')}}" class="btn btn-danger btn-sm">Excluir</a>
                     </td>
                   </tr>
                 @endforeach
                </tbody>
              </table>
            </div>
+           @endif
     </div>
 @endsection
